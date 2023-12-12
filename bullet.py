9a9327263e096da70878ad9f0ad28b78c9a13b8a
Astal2307 <151533154+Astal2307@users.noreply.github.com>
2023-12-12 18:45:41 +0400

    Add files via upload

--- bullet.py
+++ bullet.py
@@ -10,5 +10,10 @@ class Bullet(pg.sprite.Sprite):
         self.color = (168, 230, 29)
         self.speed = 10
         self.rect.centerx = player.rect.centerx
-        self.rect.top = player.rect.top
+        self.rect.top = player.rect.top + 25
         self.y = self.rect.y
+
+    def update(self):
+        self.y -= self.speed
+        self.rect.y = self.y
+        pg.draw.rect(self.screen, self.color, self.rect)
--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 import sys
 import pygame as pg
 from player import Player
+from bullet import Bullet
 
 
 def main():
@@ -16,6 +17,11 @@ def main():
 
     running = True
     moving = False
+
+    move = 0
+    bullet = None
+    bullets = []
+
     while running:
         for event in pg.event.get():
             if event.type == pg.QUIT:
@@ -26,17 +32,35 @@ def main():
                     move = -10
                 if event.key == pg.K_RIGHT:
                     move = 10
+                if event.key == pg.K_SPACE:
+                    bullet = Bullet(screen, player)
+                    bullets.append(bullet)
             if event.type == pg.KEYUP:
+                move = 0
                 moving = False
 
         screen.fill('black')
+        if bullets:
+            for blt in bullets:
+                blt.update()
         if moving:
             player.update(move=move)
         else:
             player.update()
+        remove_bullets(bullets)
         pg.display.flip()
         clock.tick(FPS)
 
+
+def remove_bullets(bullets):
+    for i in bullets.copy():
+        if i.rect.bottom <= 0:
+            bullets.remove(i)
+
+
+def create_enemies(screen, ino):
+    """Creating enemies (will need Alien class)"""
+    pass
 
 
 if __name__ == '__main__':
